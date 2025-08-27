a7b744286bf6d4a499a43623d62880eda0c7a95d
robinbood <mercedesfarts@gmail.com>
2025-08-27 22:23:23 +0500

    initial commit

--- src/Backend/Signin.tsx
+++ src/Backend/Signin.tsx
@@ -1,1 +1,11 @@
-import
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/bun-sql";
+import { SQL } from "bun";
+import { RedisClient } from "bun";
+import { users } from "@/Schema/Schema";
+const store = new RedisClient();
+const client = new SQL(Bun.env.DATABASE_URL!)
+const db = drizzle({client})
+
+
+
