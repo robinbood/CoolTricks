e9623600d8e99efcfa55e2f33b74d63572f1f74b
robinbood <mercedesfarts@gmail.com>
2025-08-27 18:08:59 +0500

    initial commit

---
+++ src/Schema/Schema.ts
@@ -0,0 +1,8 @@
+import {integer,varchar,pgTable} from "drizzle-orm/pg-core"
+
+export const users = pgTable("users",{
+    id:integer().generatedAlwaysAsIdentity().primaryKey(),
+    name:varchar({length:255}),
+    username:varchar({length:255}).notNull().unique(),
+    password:varchar({length:255}).notNull()
+})
