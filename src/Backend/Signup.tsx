7f883bb7ab7760de3b886e080d4d2535efaeb87c
robinbood <mercedesfarts@gmail.com>
2025-08-27 18:45:46 +0500

    initial commit

--- src/Backend/Signup.tsx
+++ src/Backend/Signup.tsx
@@ -1,26 +1,26 @@
-import {drizzle} from "drizzle-orm/bun-sql"
-import { eq } from "drizzle-orm"
-import { SQL } from "bun"
-import { users } from "@/Schema/Schema"
+import { drizzle } from "drizzle-orm/bun-sql";
+import { eq } from "drizzle-orm";
+import { SQL } from "bun";
+import { users } from "@/Schema/Schema";
 
-const client = new SQL(process.env.DATABASE_URL!)
-const db = drizzle({client})
+const client = new SQL(process.env.DATABASE_URL!);
+const db = drizzle({ client });
 
+const SignupUser = async (req: Request) => {
+  const { name, username, password } = await req.json();
 
-
-const SignupUser = async (req : Request) => {
-    const {name,username,password } = await req.json()
-
-    const exists  = await db.select().from(users).where(eq(username,users.username))
-    if (exists.length > 0) {
-        return new Response("Username already exists",{status:400})
-    }
-    const newPassword = await Bun.password.hash(password,{
-        algorithm:"argon2id",
-        memoryCost:10,
-         
-    })
-    await db.insert(users).values({name,username,password:newPassword})
-    return new Response("User Created Successfully",{status:201})
-}
+  const exists = await db
+    .select()
+    .from(users)
+    .where(eq(username, users.username));
+  if (exists.length > 0) {
+    return new Response("Username already exists", { status: 400 });
+  }
+  const newPassword = await Bun.password.hash(password, {
+    algorithm: "argon2id",
+    memoryCost: 10,
+  });
+  await db.insert(users).values({ name, username, password: newPassword });
+  return new Response("User Created Successfully", { status: 201 });
+};
 export default SignupUser;
