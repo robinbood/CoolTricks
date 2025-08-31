9d4c0e3b2680bd42c6cd91d8b90e23bb3c3f7ad8
robinbood <mercedesfarts@gmail.com>
2025-09-01 00:22:36 +0500

    initial commit

--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -35,9 +35,60 @@ const SignUp = () => {
             },
             body:JSON.stringify(data)
         })
-    } catch (error) {
+
+        if (response.status === 201) {
+            setTimeout(() => {
+                navigate("/api/Signin")
+            },1000)
+            const rec = await response.json()
+            SetResponse(rec.message)
+            setTimeout(()=> {
+                SetResponse("")
+            },1200)
+        }else{
+            const errorText = await response.json()
+            SetResponse(errorText.message)
+            setTimeout(() => {
+                SetResponse("")
+            },2000)
+        }
+    } catch (error : unknown) {
+        console.log("Network Error: ", error);
         
     }
+
+
+    return (
+        <div className="Signup-form" >
+            <h2 className="heading" ><span>Enter</span> your Account</h2>
+            <form onSubmit={handleSubmit(WhenSubmit)}>
+                <input {...register("name",{
+                    required:false,
+                    minLength:{
+                        value:4,
+                        message:"Way too short"
+
+                    }
+                })} placeholder="Name" className={errors.name?.message ? "error" : ""}/>
+                <input {...register("username",{
+                    required:"This is important",
+                    minLength:{
+                        value:4,
+                        message:"Too short"
+                    }
+                })} placeholder="Username" className={errors.username?.message  ?  "error" : ""} />
+                <input {...register("password",{
+                    required:"This is important",
+                    minLength:{
+                        value:8,
+                        message:"Weak password"
+                    }
+                })} placeholder="Password" className={errors.password?.message ? "error" : ""}/>
+                <input type="submit"  />
+            </form>
+
+        </div>
+    )
   };
 };
 
