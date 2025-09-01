f4d2b5db78ffe933887ada45748d1fcc6e6329a8
robinbood <mercedesfarts@gmail.com>
2025-09-01 11:43:43 +0500

    initial commit

--- src/Pages/Signin.tsx
+++ src/Pages/Signin.tsx
@@ -1,88 +1,81 @@
 import { useForm } from "react-hook-form";
-import type { SubmitHandler } from "react-hook-form";
-import { Link, useNavigate } from "react-router";
+import type { SubmitHandler} from "react-hook-form";
+import {Link, useNavigate } from "react-router";
 import { useState } from "react";
 
 interface Info {
-  name?: string;
   username: string;
   password: string;
 }
 
-const SignUp = () => {
+const SignIn = () => {
   const [response, SetResponse] = useState<string>("");
 
   const {
     register,
-    formState: { errors },
     handleSubmit,
+    formState: { errors },
+    reset
   } = useForm<Info>({
     defaultValues: {
-      name: "",
       username: "",
       password: "",
     },
   });
 
-  const navigate = useNavigate();
+  const navigate  = useNavigate()
 
-  const WhenSubmit: SubmitHandler<Info> = async (data) => {
+  const WhenSubmit: SubmitHandler<Info> = async(data) => {
     try {
-      const response = await fetch("http://localhost:3000/api/Signup", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.status === 201) {
-        setTimeout(() => {
-          navigate("/api/Signin");
-        }, 1000);
-        const rec = await response.json();
-        SetResponse(rec.message);
-        setTimeout(() => {
-          SetResponse("");
-        }, 1200);
-      } else {
-        const errorText = await response.json();
-        SetResponse(errorText.message);
-        setTimeout(() => {
-          SetResponse("");
-        }, 2000);
-      }
+        const response = await fetch("http://localhost:3000/api/Signin",{
+            method:"POST",
+            headers:{
+                "Content-type": "application/json"
+            },
+            body:JSON.stringify(data)
+        })
+        if (response.status=== 200) {
+            const res = await  response.json()
+            SetResponse(res.message)
+            setTimeout(() => {
+                reset();
+            })
+            setTimeout(() => {
+                navigate(
+                    "/api/home"
+                )
+            },2000)
+            setTimeout(() => {
+                SetResponse("")
+            })
+            
+            
+        } else {
+            const res = await response.json()
+            SetResponse(res.message)
+            setTimeout(() => {
+                SetResponse("")
+            },2000)
+        }
     } catch (error: unknown) {
-      console.log("Network Error: ", error);
-    }
+        console.log("Unknown error:",error);
+        
+    }    
 
-    return (
-      <div className="Signup-form">
+  }
+
+  return (
+    <div className="Signup-form">
         <h2 className="heading">
           {response ? (
             response
           ) : (
             <>
-              <span>Enter</span> your details
+              <span>Enter</span> your Info
             </>
           )}
         </h2>{" "}
         <form onSubmit={handleSubmit(WhenSubmit)}>
-          <input
-            {...register("name", {
-              required: false,
-              minLength: {
-                value: 4,
-                message: "Way too short",
-              },
-            })}
-            placeholder="Name"
-            className={errors.name?.message ? "error" : ""}
-          />
-
-          {errors.name?.message && (
-            <span className="error-message">{errors.name.message}</span>
-          )}
           <input
             {...register("username", {
               required: "This is important",
@@ -114,8 +107,8 @@ const SignUp = () => {
           <input type="submit" />
         </form>
       </div>
-    );
-  };
+  )
+
 };
 
-export default SignUp;
+export default SignIn;
