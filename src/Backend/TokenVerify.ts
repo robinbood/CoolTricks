466b795626afcc4a014f37659b4e48f92fe796fb
robinbood <mercedesfarts@gmail.com>
2025-09-18 23:09:33 +0500

    updating UI

--- src/Backend/TokenVerify.ts
+++ src/Backend/TokenVerify.ts
@@ -7,30 +7,47 @@ const client = new SQL(process.env.DATABASE_URL!);
 const db = drizzle(client, { schema: { users, tokens } });
 
 const TokenVerify = async (req: Request) => {
-    const {token} = await req.json()
+  const { token, newPass } = await req.json();
 
+  const result = await db.query.tokens.findFirst({
+    where: eq(tokens.token, token),
+    with: {
+      user: users,
+    },
+  });
 
-    const result = await db.query.tokens.findFirst({
-        where : eq(tokens.token,token),
-        with : {
-            user:users
-        }
-    })
+  if (!result) {
+    return Response.redirect("/signup");
+  }
 
-    if (!result) {
-        return Response.redirect("/signup")
+  const user = result.user;
+
+  const hashedpass = await Bun.password.hash(newPass, {
+    algorithm: "argon2id",
+    memoryCost: 10,
+    timeCost: 5,
+  });
+  await db.transaction(async (tx) => {
+    await tx
+      .update(users)
+      .set({ password: hashedpass })
+      .where(eq(users.id, user));
+
+    
+    await tx.delete(tokens).where(eq(tokens.token, token));
+  });
+
+  return new Response(
+    JSON.stringify({
+      message: "Password updated successfully",
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-type": "application/json",
+      },
     }
-
-    const u
-    
-
-    return new Response(JSON.stringify({
-       message:"" 
-    }))
-
-    
-
-
-} 
+  );
+};
 
 export default TokenVerify;
