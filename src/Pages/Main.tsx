2c9744a596f58fe026e7e2c956e8d1bcac41efb3
robinbood <mercedesfarts@gmail.com>
2025-10-01 11:54:20 +0500

    initial commt

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -2,11 +2,20 @@ import "../CSS/Main.css";
 
 const Main = () => {
   return (
-  <div>
-        <nav>
-
-        </nav>
-  </div>
-)}
+    <div>
+      <nav>
+        <ol>
+          <li>
+            <a href="#">Bikes</a>
+          </li>
+          <li>
+            <a href="#">BMX</a>
+          </li>
+          <li>Jump Bike 3000</li>
+        </ol>
+      </nav>
+    </div>
+  );
+};
 
 export default Main;
